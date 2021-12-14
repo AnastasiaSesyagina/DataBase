e0c8ea712f2111c59aad41b6e19d39c7ce24c09a
AnastasiaSesyagina <sesyagina@yandex.ru>
2021-12-15 01:30:47 +0300

    KR code completed

--- DataBase/KP/Task3.sql
+++ DataBase/KP/Task3.sql
@@ -1,3 +1,3 @@
-﻿--Сколько времени доставляли каждый заказ
+﻿--Показать, сколько времени доставляли каждый заказ.
 SELECT OrderID, DATEDIFF(MINUTE, GetTime, DeliveryTime) AS [MinutesToGet]
 FROM [Order];
