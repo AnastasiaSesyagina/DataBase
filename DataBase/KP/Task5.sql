4881de9f8b83d8320aaf176cc209c4ff7ef2ec74
AnastasiaSesyagina <sesyagina@yandex.ru>
2021-12-15 01:30:47 +0300

    KR code completed

---
+++ DataBase/KP/Task5.sql
@@ -0,0 +1,6 @@
+﻿--Показать стоимость заказа выбранного клиента.
+SELECT Per.FullName AS Customer, SUM(Pr.Price) AS TotalPrice
+FROM Person AS Per JOIN [Order] AS O ON Per.PersonID = O.CustomerID JOIN OrderProduct AS OP ON O.OrderID = OP.OrderID 
+JOIN Product AS Pr ON OP.ProductID = Pr.ProductID
+WHERE Per.FullName LIKE 'Сальникова Кира Вадимовна'
+GROUP BY Per.FullName;
