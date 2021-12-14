6967bd1dbd6f7b62103dd0fa88f030d4bd2a6deb
AnastasiaSesyagina <sesyagina@yandex.ru>
2021-12-15 02:15:40 +0300

    KP code completed

---
+++ DataBase/KP/KP/Task2.sql
@@ -0,0 +1,5 @@
+﻿--Показать, какие пиццы заказали клиенты.
+SELECT Per.FullName, Pr.MenuName
+FROM Person AS Per JOIN [Order] ON Per.PersonID = [Order].CustomerID JOIN OrderProduct ON [Order].OrderID = OrderProduct.OrderID 
+JOIN Product AS Pr ON OrderProduct.ProductID = Pr.ProductID
+WHERE Pr.MenuName LIKE 'Пицца%';
