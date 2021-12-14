9d0d5395c702912ad83d897dfe4a299247b77981
AnastasiaSesyagina <sesyagina@yandex.ru>
2021-12-15 02:15:40 +0300

    KP code completed

---
+++ DataBase/KP/KP/Task6.sql
@@ -0,0 +1,4 @@
+﻿--Показать, кто успел сделать заказ до полудня (открытие в 8:00).
+SELECT Per.FullName, O.GetTime
+FROM Person AS Per JOIN [Order] AS O ON Per.PersonID = O.CustomerID
+WHERE O.GetTime LIKE '08:%' OR O.GetTime LIKE '09:%' OR O.GetTime LIKE '10:%' OR O.GetTime LIKE '11:%';
