8d08b268777ee20603f57e2f000a5c1a6df8aa66
AnastasiaSesyagina <sesyagina@yandex.ru>
2021-12-15 02:27:20 +0300

    Deleting

--- DataBase/KP/KP/Task3.sql
+++
@@ -1,3 +0,0 @@
-﻿--Показать, сколько времени доставляли каждый заказ
-SELECT OrderID, DATEDIFF(MINUTE, GetTime, DeliveryTime) AS [MinutesToGet]
-FROM [Order];
--- DataBase/KP/Task4.sql
+++
@@ -1,4 +0,0 @@
-﻿--Показать, кому доставил заказы выбранный курьер.
-SELECT P1.FullName AS Courier, P2.FullName AS Customer
-FROM Person AS P1 JOIN [Order] AS O1 ON O1.CourierID = P1.PersonID JOIN Person AS P2 ON O1.CustomerID = P2.PersonID
-WHERE P1.FullName LIKE 'Дмитриев Павел Владимирович';
