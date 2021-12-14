c219260ac20f2e308756d140ba58a327883ba416
AnastasiaSesyagina <sesyagina@yandex.ru>
2021-12-14 15:28:01 +0300

    KP patr1

---
+++ DataBase/KP/Task1.sql
@@ -0,0 +1,4 @@
+﻿--Показать содержимое выбранного заказа.
+SELECT O.OrderID, P.MenuName
+FROM [Order] AS O JOIN OrderProduct ON O.OrderID = OrderProduct.OrderID JOIN Product AS P ON OrderProduct.ProductID = P.ProductID
+WHERE O.OrderID = 9;
